b7594cc2d518f8930cab26e00bba3a9833a58afe
Jash Pola <jpola@purdue.edu>
2024-06-24 15:37:56 -0400

    yeah

--- source/frame_tracker.sv
+++ source/frame_tracker.sv
@@ -11,6 +11,7 @@ module frame_tracker (
 
 logic [16:0][12:0][2:0] frame, next_frame;
 logic [3:0] current_X, next_X, current_Y, next_Y;
+logic diff_temp;
 
 generate 
     for(genvar i = 0; i < 16; i = i + 1) begin
@@ -62,38 +63,38 @@ always_comb begin
 
     if((current_X == 4'd0) || (current_X == 4'd15) || (current_Y == 4'd0) || (current_Y == 4'd11)) begin
         if(frame[current_X][current_Y] != border_c && border) begin
-            diff = 1'b1;
+            diff_temp = 1'b1;
             next_frame[current_X][current_Y] = border_c;
             obj_code = border_c;
         end
         else begin
-            diff = 1'b0;
+            diff_temp = 1'b0;
             next_frame[current_X][current_Y] = frame[current_X][current_Y];
             obj_code = frame[current_X][current_Y];
         end
     end
     else if(body && frame[current_X][current_Y] != snake_body) begin
-        diff = 1'b1;
+        diff_temp = 1'b1;
         next_frame[current_X][current_Y] = snake_body;
         obj_code = snake_body;
     end
     else if(head && frame[current_X][current_Y] != snake_head) begin
-        diff = 1'b1;
+        diff_temp = 1'b1;
         next_frame[current_X][current_Y] = snake_head;
         obj_code = snake_head;
     end
     else if(apple && frame[current_X][current_Y] != apple_c) begin
-        diff = 1'b1;
+        diff_temp = 1'b1;
         next_frame[current_X][current_Y] = apple_c;
         obj_code = apple_c;
     end
-    else if(~(apple || head || body || border) && frame[current_X][current_Y] != blank) begin
-        diff = 1'b1;
+    else if(~(apple || head || body || border) && (frame[current_X][current_Y] != blank)) begin
+        diff_temp = 1'b1;
         next_frame[current_X][current_Y] = blank;
         obj_code = blank;
     end
     else begin
-        diff = 1'b0;
+        diff_temp = 1'b0;
         next_frame[current_X][current_Y] = frame[current_X][current_Y];
         obj_code = frame[current_X][current_Y];
     end
@@ -101,5 +102,6 @@ end
 
 assign x = current_X;
 assign y = current_Y;
+assign diff = diff_temp;
 
 endmodule
