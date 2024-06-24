968b3b62fc4110d61ade0a13ef9322a5d25398c3
Jash Pola <jpola@purdue.edu>
2024-06-24 15:41:36 -0400

    yeah

--- source/frame_tracker.sv
+++ source/frame_tracker.sv
@@ -11,7 +11,7 @@ module frame_tracker (
 
 logic [16:0][12:0][2:0] frame, next_frame;
 logic [3:0] current_X, next_X, current_Y, next_Y;
-logic diff_temp;
+logic next_d, d;
 
 generate 
     for(genvar i = 0; i < 16; i = i + 1) begin
@@ -32,16 +32,17 @@ always_ff @(posedge clk, negedge nrst) begin
     if(~nrst) begin
         current_X <= 4'b0;
         current_Y <= 4'b0;
+        d <= 1'b0;
     end
     else begin
         current_X <= next_X;
         current_Y <= next_Y;
+        d <= next_d;
     end
 
 end
 
 always_comb begin
-    diff_temp = 1'b0;
     if(enable) begin
         if((current_X == 4'd15) && (current_Y == 4'd11)) begin
             next_X = 4'b0;
@@ -63,38 +64,38 @@ always_comb begin
 
     if((current_X == 4'd0) || (current_X == 4'd15) || (current_Y == 4'd0) || (current_Y == 4'd11)) begin
         if(frame[current_X][current_Y] != border_c && border) begin
-            diff_temp = 1'b1;
+            next_d = 1'b1;
             next_frame[current_X][current_Y] = border_c;
             obj_code = border_c;
         end
         else begin
-            diff_temp = 1'b0;
+            next_d = 1'b0;
             next_frame[current_X][current_Y] = frame[current_X][current_Y];
             obj_code = frame[current_X][current_Y];
         end
     end
     else if(body && frame[current_X][current_Y] != snake_body) begin
-        diff_temp = 1'b1;
+        next_d = 1'b1;
         next_frame[current_X][current_Y] = snake_body;
         obj_code = snake_body;
     end
     else if(head && frame[current_X][current_Y] != snake_head) begin
-        diff_temp = 1'b1;
+        next_d = 1'b1;
         next_frame[current_X][current_Y] = snake_head;
         obj_code = snake_head;
     end
     else if(apple && frame[current_X][current_Y] != apple_c) begin
-        diff_temp = 1'b1;
+        next_d = 1'b1;
         next_frame[current_X][current_Y] = apple_c;
         obj_code = apple_c;
     end
     else if(~(apple || head || body || border) && (frame[current_X][current_Y] != blank)) begin
-        diff_temp = 1'b1;
+        next_d = 1'b1;
         next_frame[current_X][current_Y] = blank;
         obj_code = blank;
     end
     else begin
-        diff_temp = 1'b0;
+        next_d = 1'b0;
         next_frame[current_X][current_Y] = frame[current_X][current_Y];
         obj_code = frame[current_X][current_Y];
     end
@@ -102,6 +103,6 @@ end
 
 assign x = current_X;
 assign y = current_Y;
-assign diff = diff_temp;
+assign diff = d;
 
 endmodule
