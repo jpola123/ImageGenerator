ee08fa34ad7b2e8e6784ef3eb642af815282a758
Jash Pola <jpola@purdue.edu>
2024-06-25 16:52:13 -0400

    yeah

--- source/tb_test1.sv
+++ source/tb_test1.sv
@@ -229,11 +229,12 @@ module tb_test1();
         @(posedge tb_clk);
         cmd_done = 1'b0;
 
-        for(integer i = 0; i < 192; i = i + 1) begin
+        for(integer i = 0; i < 384; i = i + 1) begin
             border = 1'b0;
             apple = 1'b0;
             snakeBody = 1'b0;
             snakeHead = 1'b0;
+            #(CLK_PERIOD);
             if((x == 4'd0) || (x == 4'd15) || (y == 4'd0) || (y == 4'd11)) begin
                 border = 1'b1;
             end
