f540f3fe93b5d8cc96c3448bfe831df26536cf1d
Jash Pola <jpola@purdue.edu>
2024-06-27 22:36:27 -0400

    yeah

--- source/tb_pixel_updater.sv
+++ source/tb_pixel_updater.sv
@@ -32,7 +32,7 @@ module tb_pixel_updater();
         #(CLK_PERIOD / 2.0); 
     end
 
-    pixel_updater DUT(.init_cycle(init_cycle), .en_update(en_update), .clk(tb_clk), .nrst(nrst), .x(x), .y(y), .obj_code(obj_code), .wr(wr), .cmd_done(cmd_done), .dcx(dcx), .D(D), .mode_o(mode_o));
+    pixel_updater DUT(.init_cycle(init_cycle), .en_update(en_update), .clk(tb_clk), .nrst(nrst), .x(x), .y(y), .obj_code(obj_code), .wr(wr), .cmd_done(cmd_done), .dcx(dcx), .D(D));
 
     initial begin
         $dumpfile("dump.vcd");
@@ -82,7 +82,7 @@ module tb_pixel_updater();
 
         #(CLK_PERIOD * 1700);
 
-        
+
 
         $finish;
     
