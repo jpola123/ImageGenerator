89316a1d4a0eb85bc41fe651cd246570065aa2ea
Jash Pola <jpola@purdue.edu>
2024-07-02 09:40:39 -0400

    yeah

--- source/variable_clock_divider.sv
+++ source/variable_clock_divider.sv
@@ -26,11 +26,11 @@ always_ff @(posedge clk, negedge nrst)
 always_comb begin
     case(mode)
     FAST: 
-        MAX_COUNT = 1;
+        MAX_COUNT = 8;
     SLOW:
-        MAX_COUNT = 1;
+        MAX_COUNT = 8;
     default: 
-        MAX_COUNT = 1;
+        MAX_COUNT = 8;
     endcase
     
     if(count >= (MAX_COUNT - 1)) begin
