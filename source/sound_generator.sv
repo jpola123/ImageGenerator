c064dca60de0ed1ed00da6ed330d79f9614565e2
Bryan Chiang <90701819+bchiang100@users.noreply.github.com>
2024-07-02 16:19:46 -0400

    Update sound_generator.sv

--- source/sound_generator.sv
+++ source/sound_generator.sv
@@ -1,25 +1,29 @@
-
+//typedef enum logic {
+    //OFF = 1'b0,
+    //ON = 1'b1
+//} MODE_TYPES;
 
 module sound_generator
 #(
     parameter N = 8
 )
 (
-    input logic clk, nRst, goodColl_i, badColl_i, button_i,
+    input logic clk, rst, goodColl_i, badColl_i, button_i,
     input logic [3:0] direction_i,
-    output logic [N - 1:0] dacCount
+    output logic [N - 1:0] soundOut
 );
 
-logic [8:0] freq;
-logic goodColl, badColl, button;
-logic [3:0] direction;
-logic playSound;
-logic at_max;
-MODE_TYPES mode_o;
+    logic goodColl, badColl, toggleMode;
+    logic [3:0] newDirection;
 
-sound_posedge_detector posDet (.goodColl(goodColl), .badColl(badColl), .button(button), .direction(direction), .clk(clk), .nRst(nRst), .goodColl_i(goodColl_i), .badColl_i(badColl_i), .button_i(button_i), .direction_i(direction_i));
-freq_selector_12M freqSel(.freq(freq), .goodColl_i(goodColl_i), .badColl_i(badColl_i), .direction_i(direction_i));
-sound_fsm fsm (.playSound(playSound), .mode_o(mode_o), .clk(clk), .nRst(nRst), .goodColl(goodColl), .badColl(badColl), .button(button), .direction(direction));
-oscillator osc (.at_max(at_max), .clk(clk), .nRst(nRst), .freq(freq), .state(mode_o), .playSound(playSound));
-dac_counter dac (.dacCount(dacCount), .clk(clk), .nRst(nRst), .at_max(at_max));
+    logic [7:0] freq;
+    logic playSound;
+    MODE_TYPES mode_o;
+    logic at_max;
+
+    posedge_detector posDetector1 (.clk(clk), .nRst(~rst), .button_i(toggleMode_i), .button(toggleMode), .goodColl_i(goodColl_i), .badColl_i(badColl_i), .direction_i(direction_i), .goodColl(goodColl), .badColl(badColl), .direction(newDirection));
+    freq_selector_12M freq_12 (.freq(freq), .goodColl_i(goodColl_i), .badColl_i(badColl_i), .direction_i(direction_i));
+    sound_fsm fsm1 (.playSound(playSound), .mode_o(mode_o), .clk(clk), .nRst(~rst), .goodColl(goodColl), .badColl(badColl), .button(toggleMode), .direction(newDirection));
+    oscillator osc1 (.at_max(at_max), .clk(clk), .nRst(~rst), .freq(freq), .state(mode_o), .playSound(playSound));
+    dac_counter dac1 (.dacCount(soundOut), .clk(clk), .nRst(~rst), .at_max(at_max));
 endmodule
