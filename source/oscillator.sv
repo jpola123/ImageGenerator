6c2687f288e6c4cbeed5e7b60f2c9385cd14ef37
Bryan Chiang <90701819+bchiang100@users.noreply.github.com>
2024-07-03 11:00:40 -0400

    Update oscillator.sv

--- source/oscillator.sv
+++ source/oscillator.sv
@@ -15,31 +15,46 @@ module oscillator
     output logic at_max
 );
 logic [N - 1:0] count, count_nxt;
-logic at_max_nxt;
+logic [19:0] stayCount, stayCount_nxt;
+logic at_max_nxt, keepCounting, keepCounting_nxt;
 always_ff @(posedge clk, negedge nRst) begin
     if (~nRst) begin
         count <= 0;
         at_max <= 0;
+        stayCount <= 0;
+        keepCounting <= 0;
     end else begin
         count <= count_nxt;
         at_max <= at_max_nxt;
+        stayCount <= stayCount_nxt;
+        keepCounting <= keepCounting_nxt;
     end
 end
 
 always_comb begin
     at_max_nxt = at_max;
     count_nxt = count;
+    keepCounting_nxt = keepCounting;
+    stayCount_nxt = stayCount;
     if (at_max == 1'b1) begin
         at_max_nxt = 1'b0;
     end
-    if (state == ON && playSound) begin
-        if (count < freq) begin
-            count_nxt = count + 1;
-        end else if (count >= freq) begin 
-            at_max_nxt = 1'b1;
-            count_nxt = 0;
+    if (playSound) begin
+        keepCounting_nxt = 1'b1;
+    end
+    if (keepCounting_nxt) begin
+        if (stayCount < 1000000) begin
+            if (count < freq) begin
+                count_nxt = count + 1;
+            end else if (count >= freq) begin 
+                at_max_nxt = 1'b1;
+                count_nxt = 0;
+            end
+            stayCount_nxt = stayCount + 1;
+        end else begin
+            keepCounting_nxt = 1'b0;
         end
-    end else if (state == OFF || ~playSound) begin
+    end else if (~keepCounting_nxt) begin
         count_nxt = 0;
         at_max_nxt = 1'b0;
     end
