cf87629786a98357aad9980c4c29e51a13955988
Samuel A Gutterman <sgutterm@ee65lnx24.ecn.purdue.edu>
2024-07-03 16:27:32 -0400

    obj plz work

--- source/obstaclegen2.sv
+++ source/obstaclegen2.sv
@@ -106,7 +106,7 @@ module obstaclegen2 #(parameter MAX_LENGTH = 50) (
 
                     if (randError == 0) begin
                         isArraySet = 1;
-                        if(curr_length > 4 && obsCount8 < curr_length + 1)
+                        if(curr_length < 4 || obsCount8 < curr_length + 1)
                             nextObstacleArray[randCordCombo] = 1;
                             nextObstacleCount = obstacleCount + 1;
                     end else begin
